c9f9b1bfac2e9e9cc75bf7688a08775b96ceb449
Dung <dung.dao19@outlook.com>
2020-06-05 21:57:57 -0500

    second commit

--- app.js
+++ app.js
@@ -84,3 +84,35 @@ const warriorsGames = [{
   }
 ]
 
+const makeChart = (games, targetTeam) => {
+  const ulParent = document.createElement('ul');
+  for(let game of games) {
+    const gameLi = document.createElement('li');
+    gameLi.innerHTML = getScoreLine(game);
+    gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'lose');
+    ulParent.appendChild(gameLi);
+  }
+  return ulParent
+};
+
+const isWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+  const target = homeTeam.team  === targetTeam ? homeTeam : awayTeam
+  return target.isWinner
+}
+
+const getScoreLine = ({ homeTeam, awayTeam }) => {
+  const {team: hTeam, points: hPoints} = homeTeam
+  const {team: aTeam, points: aPoints} = awayTeam
+  const teamNames = `${aTeam} @ ${hTeam}`
+  let scoreLine;
+  if(aPoints > hPoints) {
+    scoreLine = `<b>${aPoints}</b> - ${hPoints}`
+  } else {
+    scoreLine = `${aPoints} - <b>${hPoints}</b>`
+  }
+  return `${teamNames}: ${scoreLine}`
+};
+
+const chart1 = makeChart(warriorsGames, "Golden State");
+const section = document.querySelector('section');
+section.prepend(chart1);
